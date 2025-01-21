cb8873610429021e5956dd3754fa3b95f6703f6f
James Frost <james.frost3@snhu.edu>
2025-01-20 23:04:09 -0500

    Customized Identity Tables

--- InventoryManagerApp/Data/ApplicationDbContext.cs
+++ InventoryManagerApp/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,5 +6,19 @@ namespace InventoryManagerApp.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.HasDefaultSchema("dbo");
+
+            builder.Entity<ApplicationUser>(entity => entity.ToTable(name: "Users"));
+            builder.Entity<IdentityUserRole<string>>(entity => entity.ToTable(name: "UserRoles"));
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.ToTable(name: "UserLogins"));
+            builder.Entity<IdentityUserClaim<string>>(entity => entity.ToTable(name: "UserClaims"));
+            builder.Entity<IdentityRole>(entity => entity.ToTable(name: "Roles"));
+            builder.Entity<IdentityRoleClaim<string>>(entity => entity.ToTable(name: "RoleClaims"));
+            builder.Entity<IdentityUserToken<string>>(entity => entity.ToTable(name: "UserTokens"));
+        }
     }
 }
