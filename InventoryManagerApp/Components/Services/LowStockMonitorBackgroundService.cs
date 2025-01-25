0f510517549f20896e1ea6171aebc6c68a81a2f5
jfrostsnhu <james.frost3@snhu.edu>
2025-01-25 06:19:26 -0500

    Refactored User Management with styles and customizations.

--- InventoryManagerApp/Components/Services/LowStockMonitorBackgroundService.cs
+++ InventoryManagerApp/Components/Services/LowStockMonitorBackgroundService.cs
@@ -1,6 +1,9 @@
 ﻿
 namespace InventoryManagerApp.Components.Services
 {
+    /* LowStockMonitorService extends BackgroundService   *
+     *     to perform the Task execution of the the       *
+     * monitoring service for the low stock notifications */
     public class LowStockMonitorBackgroundService : BackgroundService
     {
         private readonly LowStockMonitorService _monitorService;
